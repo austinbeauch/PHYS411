4c1043f6c3d3e69a8eeec8664b84deaa22a25b4c
austinbeauch <austinbeauch@gmail.com>
2019-09-17 17:17:00 -0700

    Remove function files to a single script

--- A1/q3/pdf.m
+++
@@ -1,8 +0,0 @@
-function dist = pdf()
-n = 100000;
-r = rand(n, 1);
-m = mean(r);
-dist = r - m;
-
-end
-
--- A1/q3/q3.m
+++ A1/q3/q3.m
@@ -1,1 +1,21 @@
-central(5)
+loops = 5;
+numbers = 1000000;
+
+i = 1;
+for iloop = 1:loops
+    
+    pdf = rand(numbers, 1) - 0.5;
+    for q = 1:i-1
+        pdf = pdf + rand(numbers, 1) - 0.5; 
+    end
+    
+    pdf = pdf / sqrt(i);
+    
+    subplot(loops, 1, iloop); 
+    histogram(pdf, 100, 'EdgeColor','none'); 
+    title(iloop + " Distribution(s) / sqrt(" + i + ")"); 
+    xlabel("X"); 
+    ylabel("Frequency");
+    
+    i = i * 2;
+end
